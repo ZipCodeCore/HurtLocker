d97ebe3ae79e62d419566a2e70654618361e961c
marwamc <marwac@live.com>
2017-02-09 01:52:35 -0500

    the end is near :&

---
+++ src/main/java/HurtLocker.java
@@ -0,0 +1,180 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by mkulima on 2/8/17.
+ */
+public class HurtLocker {
+
+    MakeString makeString = new MakeString();
+    GroceryRegexEngine groceryRegexEngine = new GroceryRegexEngine();
+
+    // function to iterate over substrings and return key-value pairs
+    String[] getPairs(String dumbString){
+        //return dumbString.split(";|@|\\^|!|%|\\*");
+        return dumbString.split(";|\\^|@|\\*|%|!");
+    }
+
+    // internal getLabel pairs
+    String[] getLabels(String dumbString){
+        return dumbString.split(":");
+    }
+
+    // convert String[] to List<String[]>
+    public List<String[]> bagGroceries(String[] unSortedGroceries){
+        List<String[]> groceryBag = new ArrayList<String[]>();
+        int countBaggedGroceries = 0;
+        for(String s : unSortedGroceries){
+            groceryBag.add(getPairs(s));
+            countBaggedGroceries++;
+        }
+        return groceryBag;
+    }
+
+    // in bagged groceries, split into name and value
+    public List<String[]> splitLabelPairs(String[] unpaired){
+        List<String[]> pairs = new ArrayList<String[]>();
+        for(String s: unpaired){
+            pairs.add(getLabels(s));
+        }
+        return pairs;
+    }
+
+    // make new list of list of pairs
+    public List<List<String[]>> labelTheGroceries(List<String[]> baggedGroceries){
+        List<List<String[]>> labelledGroceries = new ArrayList<List<String[]>>();
+        for(String[] strarr : baggedGroceries){
+            labelledGroceries.add(splitLabelPairs(strarr));
+        }
+        return labelledGroceries;
+    }
+
+    // group the groceries by type
+    public List<List<String[]>> groupGroceries(Pattern pattern, List<List<String[]>> groceries){
+        List<List<String[]>> groupedGroceries = new ArrayList<List<String[]>>();
+        String oneGrocery = "";
+
+        for (List<String[]> strarr : groceries){
+
+            oneGrocery = handleMissing(strarr);
+            Matcher m = pattern.matcher(oneGrocery);
+            if (m.find()) {
+                groupedGroceries.add(strarr);
+                //System.out.println("milk!");
+            }
+        }
+        return groupedGroceries;
+    }
+
+    private String handleMissing(List<String[]> strarr){
+        String toReturn = "";
+        try{
+            toReturn = strarr.get(0)[1];
+        } catch(Exception e) {
+            toReturn = "BAD";
+        }
+        return toReturn;
+    }
+
+    // merge grouped groceries into single string array where first position is name.
+    // the other indices are filled with prices found
+    public String[] mergedPrices(List<List<String[]>> groupedGroceries){
+
+        String[] prices = new String[groupedGroceries.size() + 1];
+        prices[0] = ((groupedGroceries.get(0)).get(0))[1];
+
+        for(int b=0; b<groupedGroceries.size(); b++){
+            try {
+                prices[b+1] = ((groupedGroceries.get(b)).get(1))[1];
+            } catch (Exception e){
+                prices[b+1] = "BAD";
+            }
+        }
+        return prices;
+    }
+    /* */
+
+    // call merged prices method on grouped groceries
+    public List<String[]> finalizeGroceries(){
+        String unSortedString = makeString.dumb;  // get derrty string
+        String[] unSortedStringSplit = unSortedString.split("[(##)]");  // split derrty string
+        List<String[]> baggedGroceries = bagGroceries(unSortedStringSplit);   // split each string in string into K-V pairs
+        List<List<String[]>> labelledGroceries = labelTheGroceries(baggedGroceries); // further split the actual K-V pairs
+        List<String[]> finalizedGroceries = new ArrayList<String[]>();
+
+        for(Pattern p : groceryRegexEngine.GroceryPatterns){
+            List<List<String[]>> groupedGroceries1 = groupGroceries(p, labelledGroceries);
+            finalizedGroceries.add(mergedPrices(groupedGroceries1));
+        }
+        return finalizedGroceries;
+    }
+
+    public List<String[]> removeBadPrices(List<String[]> finalizedGroceries){
+        List<String[]> noRepeatPrices = new ArrayList<String[]>();
+        Set<String> namePrices = new HashSet<String>();
+        String[] setToString;
+        for(String[] sarr : finalizedGroceries){
+            Collections.addAll(namePrices, sarr);
+            setToString = namePrices.stream().toArray(String[]::new);
+            Arrays.sort(setToString, Collections.reverseOrder());
+            noRepeatPrices.add(setToString);
+            namePrices.clear();
+        }
+        return noRepeatPrices;
+    }
+
+    // prepare output for printing
+    public String makePrintable( String[] ls, String[] hasBad){
+        Arrays.sort(ls);
+        String toPrint1 = String.format("name: %7s", ls[ls.length-1]);
+        toPrint1 += "             ";
+        toPrint1 += String.format("seen: %d2 times", Arrays.asList(hasBad).size());
+        toPrint1 += "\n==============            =============\n";
+        for(int d=ls.length-2; d>=0; d--)
+        if(!matchBad(ls[d])) {
+            {
+                toPrint1 += String.format("Price: %7s", ls[d]);
+                toPrint1 += "            ";
+                toPrint1 += String.format("seen: %d times", matchPrice(groceryRegexEngine.Prices, hasBad));
+                toPrint1 += "\n______________            _____________\n";
+            }
+        }
+        return toPrint1;
+    }
+
+    private boolean matchBad(String x){
+        Matcher m = groceryRegexEngine.BAD.matcher(x);
+        return m.find();
+    }
+    /* */
+
+    private int matchPrice(Pattern[] pricePatterns, String[] groceryGroup){
+        int numMatches = 0;
+        for(Pattern p : pricePatterns){
+
+            for(String s : groceryGroup) {
+                Matcher m = p.matcher(s);
+                if (m.find()) {
+                    numMatches = m.group().length();
+                    //System.out.println(m.group());
+                }
+            }
+
+        }
+        return numMatches;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/HurtLockerMain.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mkulima on 2/8/17.
+ */
+public class HurtLockerMain {
+
+    public static void main(String[] args) {
+        MakeString mk = new MakeString();
+        HurtLocker ht = new HurtLocker();
+        String[] ds = (mk.dumb).split("[(##)]");
+        GroceryRegexEngine regexEngine = new GroceryRegexEngine();
+
+        List<String[]> baggedGroceries = ht.bagGroceries(ds);
+        List<List<String[]>> labelledAndBaggedGroceries = ht.labelTheGroceries(baggedGroceries);
+        List<List<String[]>> milks = ht.groupGroceries(regexEngine.Milk, labelledAndBaggedGroceries);
+
+        /*
+        for(String s2 : ds){
+            System.out.println(s2);
+        }
+        /**/
+
+        /*
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println();
+        for(int i=0; i<baggedGroceries.size(); i+=2){
+            System.out.print(Arrays.toString(baggedGroceries.get(i)) + "      " + baggedGroceries.get(i).length + " "+ baggedGroceries.size() + "\n");
+
+        }
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println();
+        /* */
+
+        /*
+        for (int j=0; j<labelledAndBaggedGroceries.size(); j+=2){
+            List<String[]> lstr = labelledAndBaggedGroceries.get(j);
+
+            System.out.println(lstr.size());
+            System.out.println(ht.makePrintable(lstr));
+        }
+        /* */
+
+        /*
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println(milks.size());
+        for (int k=0; k<milks.size(); k++){
+            List<String[]> lstr = milks.get(k);
+
+            System.out.println(k);
+            System.out.println(ht.makePrintable(lstr));
+            //System.out.println(Arrays.toString(ht.mergedPrices(lstr)));
+        }
+        /* */
+        //System.out.println(Arrays.toString(ht.mergedPrices(milks)));
+
+        /*
+        for (List<String[]> ls : labelledAndBaggedGroceries) {
+            System.out.println(" ----------------------- ");
+            for (String[] sarr : ls) {
+                System.out.print(Arrays.toString(sarr) + " ");
+            }
+            System.out.println(" ");
+        }
+        /**/
+
+        for(String[] sarr : ht.finalizeGroceries())
+            System.out.println(Arrays.toString(sarr));
+
+        System.out.println();
+        System.out.println("--------------------------------------------------------------------------");
+        List<String[]> removedBadPrices = ht.removeBadPrices(ht.finalizeGroceries());
+        List<String[]> withBadPrices = ht.finalizeGroceries();
+
+        for(int x=0; x<removedBadPrices.size(); x++){
+            ///Arrays.sort(removedBadPrices.get);
+            //System.out.println(Arrays.asList(removedBadPrices.get(x)));
+            System.out.println(ht.makePrintable(removedBadPrices.get(x), withBadPrices.get(x)));
+            System.out.println();
+
+        }
+    }
+}
