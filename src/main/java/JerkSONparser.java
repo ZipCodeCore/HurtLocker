24bc1983efe341cccbe3c30a23df1802ac68751f
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-02-09 19:34:30 -0500

    Keys spelling correctin method

--- src/main/java/JerkSONparser.java
+++ src/main/java/JerkSONparser.java
@@ -7,40 +7,153 @@ import java.util.regex.Pattern;
  */
 public class JerkSONparser
 {
-    public ArrayList<String> byGroups(String txt)
+    public ArrayList<ArrayList> byGroups(String txt)
     {
-        ArrayList<String> groupArray = new ArrayList<String>();
-        Pattern groups = Pattern.compile("[^#]+?(?=##)");
+        ArrayList<String> groupArray;// = new ArrayList<String>();
+        ArrayList<ArrayList> keysXvaluesArray = new ArrayList<ArrayList>();
+
+        Pattern groups = Pattern.compile("[^#]+");
         Matcher groupMatch = groups.matcher(txt);
 
         //StringBuffer result = new StringBuffer();
         while (groupMatch.find())
         {
+            groupArray = new ArrayList<String>();
             //result.append(groupMatch.group());
             groupArray.add(groupMatch.group());
+            keysXvaluesArray.add(groupArray);
         }
         //return result.toString();
         //return groupMatch.group();
-        return groupArray;
+        return keysXvaluesArray;
 
     }
 
-    public ArrayList<String> extractItems(ArrayList<String> groups)
+    public String formatItemsPunctuation(ArrayList<String> groupArray)
     {
-        ArrayList<String> keysValuesArray = new ArrayList<String>();
-
-        for (String group : groups
+        String fixedString = "";
+        for (String txt : groupArray
                 )
         {
-            Pattern items = Pattern.compile("[^;|:|!|#|%|^]+?(?=([;|:|!|#|%|^]))");
-            Matcher itemMatch = items.matcher(group);
+            Pattern pPattern = Pattern.compile("[;|!|#|%|^|@]");
+            Matcher mMatcher = pPattern.matcher(txt);
 
-            //StringBuffer result = new StringBuffer();
-            while (itemMatch.find()){
+            StringBuffer result = new StringBuffer();
+            while (mMatcher.find())
+            {
+                mMatcher.appendReplacement(result, ",");
+            }
+            mMatcher.appendTail(result);
+            fixedString = result.toString();
+        }
+        return fixedString;
+    }
+
+    public String correctKeysSpelling(ArrayList<String> groupArray)
+    {
+        String workingString = "";
+        String fixedString = "";
+        for (String txt : groupArray
+                )
+        {   //name
+            Pattern namePattern = Pattern.compile("((?i)(n.{2}e))");
+            Matcher nameMatcher = namePattern.matcher(txt);
+
+            StringBuffer nameBuffer = new StringBuffer();
+            while (nameMatcher.find())
+            {
+                nameMatcher.appendReplacement(nameBuffer, "name");
+            }
+            nameMatcher.appendTail(nameBuffer);
+            workingString = nameBuffer.toString();
+
+            //price
+            Pattern pricePattern = Pattern.compile("(?i)(p.{3}e)");
+            Matcher priceMatcher = pricePattern.matcher(workingString);
+
+            StringBuffer priceBuffer = new StringBuffer();
+            while (priceMatcher.find())
+            {
+                priceMatcher.appendReplacement(priceBuffer, "Price");
+            }
+            priceMatcher.appendTail(priceBuffer);
+            workingString = priceBuffer.toString();
+
+            //type
+            Pattern typePattern = Pattern.compile("(?i)(t.{2}e)");
+            Matcher typeMatcher = typePattern.matcher(workingString);
+
+            StringBuffer typeBuffer = new StringBuffer();
+            while (typeMatcher.find())
+            {
+                typeMatcher.appendReplacement(typeBuffer, "type");
+            }
+            typeMatcher.appendTail(typeBuffer);
+            workingString = typeBuffer.toString();
+
+            //expiration
+            Pattern expPattern = Pattern.compile("(?i)(e.{8}n)");
+            Matcher expMatcher = expPattern.matcher(workingString);
+            StringBuffer expBuffer = new StringBuffer();
+            while(expMatcher.find())
+            {
+                expMatcher.appendReplacement(expBuffer, "expiration");
+            }
+            expMatcher.appendTail(expBuffer);
+            workingString = expBuffer.toString();
+
+
+        }
+        return workingString;
+    }
+
+    public ArrayList<String> itemXgroupParser(String completeJerkSON)//does not work as needed
+    {
+
+        ArrayList<String> keysValuesArray = new ArrayList<String>();
+
+        Pattern groups = Pattern.compile("[^#]+(?=#)");
+        Matcher groupMatch = groups.matcher(completeJerkSON);
+        while (groupMatch.find())
+        {
+            Pattern items = Pattern.compile("[^;|:|!|#|%|^|@]+");
+            Matcher itemMatch = items.matcher(groupMatch.group());
+            while (itemMatch.find())
+            {
                 keysValuesArray.add(itemMatch.group());
+                //System.out.println(itemMatch.group());
             }
         }
         return keysValuesArray;
     }
+/*
+    public String errorCorrector(String errorString)
+    {
+        //keys grammar corrections
+        String goodString;
+        goodString =
+                errorString.replaceAll("(?i)(n.{2}e)", "name")
+                        .replaceAll("(?i)(p.{3}e)", "Price")
+                        .replaceAll("(?i)(t.{2}e)", "type")
+                        .replaceAll("(?i)(e.{8}n)", "expiration")
 
+                        //values grammar corrections
+                        .replaceAll("(?i)(m{2}k)", "Milk")//replaces all wrong 'm**k' with "Milk"
+                        .replaceAll("(?i)(b.{3}d)", "Bread")
+                        .replaceAll("(?i)(c.{5}s)", "Cookies")
+                        .replaceAll("(?i)(a.{4}s)", "Apples")
+                        .replaceAll("(?i)(f.{2}d)", "Food")
+
+                        //key and value connector corrections
+                        .replaceAll("[|:|!|%|^|@]", ":")
+
+                        //key value pairs separator corrections
+                        .replaceAll("[;]", ",");
+        return goodString;
+
+    }
+    */
 }
+
+
+
--- src/test/testClass.java
+++ src/test/testClass.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -10,12 +12,16 @@ public class testClass
 {
     String raw;
     JerkSONparser testParser;
+    ArrayList<String> groupArray;
 
     @Before
     public void setUp() throws Exception
     {
         raw = (new Main()).readRawDataToString();
         testParser = new JerkSONparser();
+        groupArray = new ArrayList<String>();
+        groupArray.add("naME:BreaD;price:1.23;type:Food@expiration:1/02/2016");
+
     }
 
     @Test
@@ -26,18 +32,40 @@ public class testClass
         int actual = testParser.byGroups(raw).size();
         int expected = 28;
         //int expected = -1;//fail test
-        assertEquals("should return groups between ## without ##", expected, actual);
+        assertEquals("should return arrays of groups between ##, without ##", expected, actual);
     }
 
     @Test
-    public void extractItemsTest()
+    public void formatItemsPunctuationTest()
     {
-        testParser.extractItems(testParser.byGroups(raw));
-        System.out.println(testParser.extractItems(testParser.byGroups(raw)));
-        int actual = testParser.extractItems(testParser.byGroups(raw)).size();
-        int expected = 8;
-        assertEquals("should return number of items...8?", expected, actual);
+        //testParser.formatItemsPunctuation(groupArray);
+        //System.out.println(testParser.formatItemsPunctuation(groupArray));
 
+        String actual = testParser.formatItemsPunctuation(groupArray);
+        String expected = "naME:BreaD,price:1.23,type:Food,expiration:1/02/2016";
+        assertEquals("should return number of items...8?", expected, actual);
+    }
+    @Test
+    public void correctKeysSpellingTest(){
+        String actual = testParser.correctKeysSpelling(groupArray);
+        String expected = "fail";//fail test
+        //String expected = "name:BreaD;price:1.23;type:Food@expiration:1/02/2016";
+        assertEquals("should return 'name' correct spelling & lower case", expected, actual);
+    }
+
+
+    @Test
+    public void itemXgroupParserTest()
+    {
+        testParser.itemXgroupParser(raw);
+        System.out.println(testParser.itemXgroupParser(raw));
+
+    }
+
+    @Test
+    public void errorCorrectorTest()
+    {
+        //System.out.println(testParser.errorCorrector(raw));
 
     }
 }
