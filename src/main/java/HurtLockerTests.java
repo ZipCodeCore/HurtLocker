a9c0dbe9f1b35354c8194b633932e1929c75579c
Mark A Brown <markabrown87@gmail.com>
2017-05-31 13:20:29 -0400

    Finished MilkPriceKeyParser

---
+++ src/main/java/HurtLockerTests.java
@@ -0,0 +1,68 @@
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HurtLockerTests {
+
+    @Test
+    public void testReadRawDataAsString() {
+        //Given:
+        ClassLoader classLoader = getClass().getClassLoader();
+        String expectedString = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
+        String result = "";
+        //When:
+        try {
+            result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        //Then:
+        Assert.assertTrue(expectedString.equals(result));
+    }
+
+    @Test
+    public void testCreateListOfFoodItems() {
+        //Given:
+        String rawData = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
+        ArrayList<String> expectedFoodList = new ArrayList<String>();
+        expectedFoodList.add("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##");
+        expectedFoodList.add("naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##");
+        expectedFoodList.add("NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##");
+
+        //When:
+        ArrayList<String> actualFoodList = Main.createListOfFoodItems(rawData);
+        System.out.println(expectedFoodList);
+        System.out.println(actualFoodList);
+
+        //Then:
+        Assert.assertTrue(expectedFoodList.equals(actualFoodList));
+    }
+
+
+
+    @Test
+    public void testParseForMilkPriceKeyPair() {
+        //Given:
+        String rawData = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##";
+        ArrayList<String> milkProductList = Main.createListOfFoodItems(rawData);
+        ArrayList<ArrayList<String>> expectedMilkProductList = new ArrayList<ArrayList<String>>();
+        ArrayList<String> milkProductOne = new ArrayList<String>();
+        milkProductOne.add("Milk");
+        milkProductOne.add("3.23");
+        ArrayList<String> milkProductTwo = new ArrayList<String>();
+        milkProductTwo.add("MiLK");
+        milkProductTwo.add("3.23");
+        expectedMilkProductList.add(milkProductOne);
+        expectedMilkProductList.add(milkProductTwo);
+
+        //When:
+        ArrayList<ArrayList<String>> actualMilkProductList = Main.parseListForMilkPriceKeyPair(milkProductList);
+
+        //Then:
+        Assert.assertTrue(expectedMilkProductList.equals(actualMilkProductList));
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Main {
 
@@ -13,5 +15,37 @@ public class Main {
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
 
+    }
+
+    public static ArrayList<String> createListOfFoodItems(String rawData) {
+        ArrayList<String> foodList = new ArrayList<String>();
+        Pattern pattern = Pattern.compile("([^#]{0,}#{2})");
+        Matcher matcher = pattern.matcher(rawData);
+        while (matcher.find()) {
+            foodList.add(matcher.group());
+        }
+        return foodList;
+    }
+
+    public static ArrayList<ArrayList<String>> parseListForMilkPriceKeyPair(ArrayList<String> foodList) {
+        ArrayList<ArrayList<String>> milkList = new ArrayList<ArrayList<String>>();
+        Pattern patternCorrect = Pattern.compile("([M|m][I|i][L|l][K|k])\\;price\\:(\\d\\.\\d{2})");
+        Pattern patternError = Pattern.compile("([M|m][I|i][L|l][K|k])\\;price\\:");
+        for (String food : foodList) {
+            ArrayList<String> milkEntry = new ArrayList<String>();
+            Matcher matcherCorrect = patternCorrect.matcher(food);
+            if (matcherCorrect.find()) {
+                milkEntry.add(matcherCorrect.group(1));
+                milkEntry.add(matcherCorrect.group(2));
+                milkList.add(milkEntry);
+            }
+            else {
+                Matcher matcherError = patternError.matcher(food);
+                if (matcherError.find()) {
+                    ErrorCounter.increaseErrorCount();
+                }
+            }
+        }
+        return milkList;
     }
 }
