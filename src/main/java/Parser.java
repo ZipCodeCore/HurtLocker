be7a277f654dbcc7b49ef6d3a27bba6a33a30249
ant <anthonyjones@zipcoder3.local>
2017-05-31 14:15:05 -0400

    Hurt Locker is hurting

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by anthonyjones on 5/31/17.
  */
@@ -8,10 +12,41 @@ public class Parser {
 //
 //    }
 
-    public String patternMatch(String file){
-        System.out.println(file);
-        return file;
+    public String patternMatch(String file) {
+        ArrayList<String> lineBreaker = lineBreaker(file);
+        System.out.println(lineBreaker);
+        Pattern pattern = Pattern.compile("(\\w+.)([M|m].\\w+[k|K])|([B|b].\\w+[D|d])|([A|a].\\w+[S|s])|([C|c].\\w+[S|s])");
+        Matcher matcher = pattern.matcher(file);
+
+        while (matcher.find()) {
+           if(matcher.find(1)){
+               System.out.println("milk");
+           }
+        }
+        System.out.println(lineBreaker);
+        return "";
     }
+
+    public ArrayList<String> lineBreaker(String file) {
+        Pattern pattern = Pattern.compile("[^#]+");
+        Matcher matcher = pattern.matcher(file);
+        ArrayList<String> arrayListOfBrokenLines = new ArrayList<>();
+
+        return arrayListOfBrokenLines;
+    }
+
+//    public String milk(){
+//        ArrayList<String> lineBreaker = lineBreaker(file);
+//        System.out.println(lineBreaker);
+//        Pattern pattern = Pattern.compile("(^\\w+.?[M|m][i|I][l|L][k|K])");
+//        Matcher matcher = pattern.matcher(file);
+//
+//        while (matcher.find()) {
+//            lineBreaker.add(matcher.group());
+//        }
+//        System.out.println(lineBreaker);
+//        return "";
+//    }
 
 
 }
--- src/test/java/testParser.java
+++ src/test/java/testParser.java
@@ -1,21 +1,49 @@
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by anthonyjones on 5/31/17.
  */
 public class testParser {
     Parser parser;
+    String output;
 
     @Before
-    public void init() {
+    public void init() throws Exception {
         parser = new Parser();
+        ClassLoader classLoader = getClass().getClassLoader();
+      //  String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+
+         output = (new Main()).readRawDataToString();
     }
 
 
     @Test
     public void testPatternMatch() {
-        
+        //Given
+        String given = output;
+        String expected = "naMe";
+        //When
+        String actual = parser.patternMatch(given);
+        //Then
+        Assert.assertEquals("This should return a String with the first product", expected, actual);
+    }
 
+
+    @Test
+    public void testLineBreaker() {
+        //Given
+        String given = output;
+        String expected = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016";
+        //When
+        ArrayList<String> arrayListOfBrokenLines = parser.lineBreaker(given);
+        String actual = arrayListOfBrokenLines.get(0);
+        //Then
+        Assert.assertEquals("This should break the line right after two hashtags", expected, actual);
     }
 }
