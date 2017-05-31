b45e90c65d830a5870d5dad880383689d1c98bc8
Aaron Long <aaron.long.c@gmail.com>
2017-05-31 13:23:08 -0400

    Added more test cases

--- src/test/java/io/github/aaronclong/hurtlocker/parser/ParsedKeyValueTest.java
+++ src/test/java/io/github/aaronclong/hurtlocker/parser/ParsedKeyValueTest.java
@@ -11,9 +11,41 @@ import java.util.HashMap;
 public class ParsedKeyValueTest {
   @Test
   public void testGetParsedWithColon() {
-    ParsedKeyValue kvPair = ParsedKeyValue.parse("naMe:Milk;");
+    ParsedKeyValue kvPair = ParsedKeyValue.parse("price:1.23;");
+    HashMap<String, String> mapSeparatedByColon = new HashMap<>();
+    mapSeparatedByColon.put("price", "1.23");
+    assertEquals("Testing getting pair with colon", mapSeparatedByColon, kvPair.getParsed());
+  }
+
+  @Test
+  public void testGetParsedWithAtSign() {
+    ParsedKeyValue kvPair = ParsedKeyValue.parse("naMe@Milk;");
     HashMap<String, String> mapSeparatedByColon = new HashMap<>();
     mapSeparatedByColon.put("naMe", "Milk");
-    assertEquals("Testing getting pair with colon", mapSeparatedByColon, kvPair.getParsed());
+    assertEquals("Testing getting pair with atSign", mapSeparatedByColon, kvPair.getParsed());
+  }
+
+  @Test
+  public void testGetParsedWithUpWardsArrow() {
+    ParsedKeyValue kvPair = ParsedKeyValue.parse("expiration^5/02/2016;");
+    HashMap<String, String> mapSeparatedByColon = new HashMap<>();
+    mapSeparatedByColon.put("naMe", "Milk");
+    assertEquals("Testing getting pair with atSign", mapSeparatedByColon, kvPair.getParsed());
+  }
+
+  @Test
+  public void testGetParsedWithAsterisk() {
+    ParsedKeyValue kvPair = ParsedKeyValue.parse("naMe*Milk;");
+    HashMap<String, String> mapSeparatedByColon = new HashMap<>();
+    mapSeparatedByColon.put("naMe", "Milk");
+    assertEquals("Testing getting pair with atSign", mapSeparatedByColon, kvPair.getParsed());
+  }
+
+  @Test
+  public void testGetParsedWithPercentSymbol() {
+    ParsedKeyValue kvPair = ParsedKeyValue.parse("naMe%Milk;");
+    HashMap<String, String> mapSeparatedByColon = new HashMap<>();
+    mapSeparatedByColon.put("naMe", "Milk");
+    assertEquals("Testing getting pair with atSign", mapSeparatedByColon, kvPair.getParsed());
   }
 }
