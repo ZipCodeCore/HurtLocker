be91f41277b58e9a53a30e6bca2ce0512bbaaf21
Aaron Long <aaron.long.c@gmail.com>
2017-05-31 12:03:07 -0400

    Fixed Tariq's mistakes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
-import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
+  public String readRawDataToString() throws Exception {
+    ClassLoader classLoader = getClass().getClassLoader();
+    return IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+  }
 
-    public String readRawDataToString() throws Exception{
-        ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-        return result;
-    }
-
-    public static void main(String[] args) throws Exception{
-        String output = (new Main()).readRawDataToString();
-        System.out.println(output);
-
-    }
+  public static void main(String[] args) throws Exception {
+    String output = (new Main()).readRawDataToString();
+    System.out.println(output);
+  }
 }
