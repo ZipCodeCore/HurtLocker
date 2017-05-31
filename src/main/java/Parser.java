b448d2ab65970c91eff882a95b120892ffa73cc5
wongandrewp <wong.andrew.p@gmail.com>
2017-05-31 15:19:08 -0400

    getItemStringsStream is awesome

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,10 @@
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -10,21 +13,41 @@ import java.util.stream.Stream;
  */
 public class Parser {
 
-    public Stream<String> getRawDataStream() throws IOException{
-        Stream<String> rawDataStream = Files.lines(Paths.get("RawData.txt"));
-        return rawDataStream;
+    public String readRawDataToString() throws IOException{
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        return result;
     }
 
-    public static Stream<String> getItemStringsStream(String rawDataString) {
-        CharSequence rawDataSequence = rawDataString.toString();
-        return Pattern.compile(".+?(?=##)").splitAsStream(rawDataSequence);
+
+
+    public Stream<String> getItemStringsStream(String rawDataString) {
+        Pattern pattern = Pattern.compile("##");
+        return pattern.splitAsStream(rawDataString);
     }
 
-//    public Stream<String> getItemStringsStream(Stream<String> rawDataStream) {
-//        Stream<String> itemStringsStream = Stream.of(rawDataStream.)
+    public Stream<String> getNamesStream(Stream<String> itemStringStream) {
+        Stream<String> namesStream;
+        namesStream = itemStringStream.peek( iS ->  getName(iS) );
+        return null;
+    }
+
+    private Stream<String> getName(String iS) {
+        //find regex for String after name:, before symbol
+        Pattern pattern = Pattern.compile("regex");
+        return pattern.splitAsStream(iS);
+    }
+
+    // used to see elements in stream for debugging purposes
+    public static String[] getItemStringsArray(String rawDataString) {
+        Pattern pattern = Pattern.compile("##");
+        return pattern.split(rawDataString);
+    }
+
+//    public Stream<String> getRawDataStream() throws IOException{
+//        Stream<String> rawDataStream = Files.lines(Paths.get("RawData.txt"));
+//        return rawDataStream;
 //    }
 
-//    public Stream<String> getFirstItemAsStream(Stream<String> rawDataStream) {
-//         firstItemStream = Stream.of(rawDataStream.findFirst());
-//    }
+
 }
--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -1,22 +1,56 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.Optional;
 import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by andrewwong on 5/31/17.
  */
 public class TestParser {
+    String rawDataString;
+    Parser parser;
+    @Before
+    public void init() {
+        parser = new Parser();
+        try{
+            rawDataString = parser.readRawDataToString();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
     @Test
     public void testGetItemStringsStream() {
         // Given
-        String rawDataString = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##\n";
+        Optional<String> expectedFirst = Optional.of("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016");
+        long expectedCount = 28;
+        // When
+        Stream<String> actualStream = parser.getItemStringsStream(rawDataString);
+        Optional<String> actualFirst = actualStream.findFirst();
+        actualStream = parser.getItemStringsStream(rawDataString);
+        long actualCount = actualStream.count();
 
-        //When
-        Stream<String> actual = Parser.getItemStringsStream(rawDataString);
-        String actualString = actual.map( n -> n.toString() ).collect(Collectors.joining(""));
+        // Then
+        assertEquals(expectedFirst, actualFirst);
+        assertEquals(expectedCount, actualCount);
+    }
 
-        System.out.println(actualString);
+    @Test
+    public void testGetNameFromItemString() {
+        // Given
+        Stream<String> itemStringStream = parser.getItemStringsStream(rawDataString);
+        Optional<String> expectedFirstName = Optional.of("Milk");
+
+        // When
+        Stream<String> nameStream = parser.getNamesStream(itemStringStream);
+        Optional<String> actualFirstName = nameStream.findFirst();
+        // Then
+        assertEquals(expectedFirstName, actualFirstName);
+
+
     }
 }
