83cf7592f9c589ae35553d782eee42575b29a1af
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-02 14:14:00 -0500

    caught counting errors, properly reflects milk

--- src/main/java/ItemCounter.java
+++ src/main/java/ItemCounter.java
@@ -1,11 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ItemCounter {
 
     public String itemCounter(String[] array, String p, String item) throws Exception {
         ContainsRegex regex = new ContainsRegex();
         String pattern = p;
         Integer count = 0;
+
+        Pattern pricePattern = Pattern.compile("(?<!(?:\\d|\\.))\\d+\\.\\d{2}(?!\\.)");
+
         for (String s : array) {
-            if (regex.containsRegex(s, pattern)) {
+            Matcher matcher = pricePattern.matcher(s);
+            if (regex.containsRegex(s, pattern) && matcher.find()) {
                 count++;
             }
         }
