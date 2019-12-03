0ba92371d2132b2a7ed3ba163b3ff81b1cc81828
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-03 01:15:33 -0500

    map created

--- src/main/java/PrintResults.java
+++ src/main/java/PrintResults.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PrintResults {
-//    CleanTheJerk cleaner = new CleanTheJerk();
-//    FindPatterns finder = new FindPatterns();
-//
-//    public void seeIfMilkReplaced () {
-//        cleaner.replaceMilk();
-//        finder.printResults();
-//    }
+    CleanTheJerk cleaner = new CleanTheJerk();
+    FindPatterns finder = new FindPatterns();
+    Map<String, Long> myMap = new HashMap();
+
+
+    public Map resultsMap (){
+        ArrayList<GroceryObjects> list = finder.createGroceryObjects();
+        for (int i = 0; i < list.size(); i++) {
+                    myMap = list.stream().filter(x -> x.getName().equals("Milk")).collect(Collectors.groupingBy(GroceryObjects::getPrice, Collectors.counting()));
+            }
+        return myMap;
+    }
 
 }
--- src/test/java/PrintResultsTest.java
+++ src/test/java/PrintResultsTest.java
@@ -1,21 +1,23 @@
 import org.junit.Before;
 import org.junit.Test;
 
-//public class PrintResultsTest {
-//    CleanTheJerk cleaner;
-//    FindPatterns finder;
-//    PrintResults printer;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        cleaner = new CleanTheJerk();
-//        finder = new FindPatterns();
-//        printer = new PrintResults();
-//    }
-//
-//    @Test
-//    public void checkMilk () {
-//        printer.seeIfMilkReplaced();
-//
-//    }
-//}
+import java.util.Map;
+
+public class PrintResultsTest {
+    CleanTheJerk cleaner;
+    FindPatterns finder;
+    PrintResults printer;
+
+    @Before
+    public void setUp() throws Exception {
+        cleaner = new CleanTheJerk();
+        finder = new FindPatterns();
+        printer = new PrintResults();
+    }
+
+    @Test
+    public void checkMilk () {
+       Map<String, Long> myMap = printer.resultsMap();
+        System.out.println(myMap.entrySet());
+    }
+}
