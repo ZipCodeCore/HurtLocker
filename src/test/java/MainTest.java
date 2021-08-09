dbe74c1e8e1adc21d890d536ffaad5d867f1abe5
johnS93 <john.song8293@gmail.com>
2021-08-09 16:25:53 -0400

    Hurting me real good

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    Main test = new Main();
+
+    @Test
+    public void findAlphaTest() {
+        String look = test.findAlphabetCharacters();
+
+        System.out.println(look);
+    }
+
+    @Test
+    public void formatTest() {
+        String input = new String();
+        String look = test.getList(input);
+
+        System.out.println(look);
+    }
+
+    @Test
+    public void findMilkTest(){
+        int actual = test.countMilk();
+        int expected = 8;
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void correctSeparatorTest(){
+        String look = test.correctSeparator();
+        System.out.println(look);
+    }
+
+    @Test
+    public void correctAndSeparateTest(){
+        String look = test.separateBySemicolon();
+        System.out.println(look);
+    }
+    @Test
+    public void printTest(){
+        System.out.println(test.doingFormatting());
+    }
+}
