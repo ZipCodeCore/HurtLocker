4e6f12134f18fd60041d0ec6115e814c8bde5aa0
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-09 15:30:58 -0400

    Lab Completed. This hurt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    Integer counter = 0;
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -11,7 +15,170 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
+        //   String actualResult = changeCookies(changeApples(changeBread(changeMilk(output))));
+        //   System.out.println(actualResult);
 
+    }
+
+    public String changeMilk (String input) {
+        try {
+            Pattern pattern = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Milk");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String changeBread (String input) {
+        try {
+            Pattern pattern = Pattern.compile("bread", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Bread");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String changeCookies (String input) {
+        try {
+            Pattern pattern = Pattern.compile("c[o0][o0]kies", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Cookies");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String changeApples (String input) {
+        try {
+            Pattern pattern = Pattern.compile("apples", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Apples");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String poundToNewLine(String input) {
+        try {
+            Pattern pattern = Pattern.compile("##");
+            Matcher matcher = pattern.matcher(input);
+            String result = matcher.replaceAll("\n");
+            return result;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String correctSeparator () {
+        // "[!@^%*]"
+        try {
+            String jawn = readRawDataToString();
+            Pattern patter = Pattern.compile("[!@^%*]");
+            Matcher matcher = patter.matcher(jawn);
+            String result = matcher.replaceAll(";");
+            return result;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String nameChange(String input) {
+        try {
+            Pattern pattern = Pattern.compile("name", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Name");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public String priceChange(String input) {
+        try {
+            Pattern pattern = Pattern.compile("price", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.replaceAll("Price");
+
+            return milk;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    // we can use a "scanner" something to check when the (';' <- those bois) come up
+
+    public int findGroceries(String input) {
+        Integer holdingValue = 0;
+        Boolean checkVal = false;
+        Pattern pattern = Pattern.compile(input, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(readyForFormatting());
+        while(!checkVal) {
+            if (!matcher.find()) {
+                checkVal = true;
+                continue;
+            }
+            holdingValue++;
+        }
+        return holdingValue;
+    }
+
+
+    public String doingTheFormatting() {
+        String result =
+                "ame:    Milk        seen: " + findGroceries("milk") + " times\n" +
+                        "=============        =============\n" +
+                        "Price:   3.23        seen: " + findGroceries("milk;price:3.23") + " times\n" +
+                        "-------------        -------------\n" +
+                        "Price:   1.23        seen: " + findGroceries("milk;price:1.23") +  " times\n\n" +
+
+                        "name:   Bread        seen: " + findGroceries("bread") + " times\n" +
+                        "=============        =============\n" +
+                        "Price:   1.23        seen: " + findGroceries("bread") + " times\n" +
+                        "-------------        -------------\n\n" +
+
+                        "name: Cookies        seen: " + findGroceries("cookies") + " times\n" +
+                        "=============        =============\n" +
+                        "Price:   2.25        seen: " + findGroceries("cookies") + " times\n" +
+                        "-------------        -------------\n\n" +
+
+                        "name:  Apples        seen: " + (findGroceries("apples;price:0.25") + findGroceries("apples;price:0.23")) + " times\n" +
+                        "=============        =============\n" +
+                        "Price:   0.25        seen: " + findGroceries("price:0.25") + " times\n" +
+                        "-------------        -------------\n" +
+                        "Price:   0.23        seen: " + findGroceries("price:0.23") + " times\n\n" +
+
+                        "Errors               seen: " + countingErrors() + " times";
+        return result;
+    }
+
+    public int countingErrors() {
+        int counter = 0;
+        // beware
+//             counter += findGroceries("apples") - (findGroceries("apples;price:0.25") + findGroceries("apples;price:0.23"));
+        counter += findGroceries("Name:;");
+        counter += findGroceries("milk") - (findGroceries("milk;price:3.23") + findGroceries("milk;price:1.23"));
+        return counter;
+    }
+
+    public String readyForFormatting() {
+        String result = poundToNewLine(correctSeparator());
+        String result1 = changeApples(result);
+        String result2 = changeBread(result1);
+        String result3 = changeCookies(result2);
+        String result4 = changeMilk(result3);
+        String result5 = nameChange(result4);
+        String result6 = priceChange(result5);
+        return result6;
     }
 }
