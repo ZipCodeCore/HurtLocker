ebfc01a54fba389c07d3e228f8332807f8f4ece2
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-13 10:22:07 -0500

    split apart the entries

--- src/main/java/RegexThis.java
+++ src/main/java/RegexThis.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -6,10 +7,9 @@ import java.util.regex.Pattern;
 public class RegexThis {
     //3 to 5 lines of regex
     private String orig;
-    public static Map<String,String> regexer(String raw){
-        Map<String,String> refined = new HashMap<String,String>();
-        ""
-        return refined;
+    public static String[] splitter(String raw){
+         return Arrays.copyOf((Pattern.compile("##")).split(raw),(Pattern.compile("##")).split(raw).length - 1);
     }
+    private static 
 
 }
