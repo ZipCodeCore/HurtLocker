3afea46c0768e6730c3624ac28bdd30d8a813024
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-13 09:13:12 -0500

    skeletized methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,5 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
-
     }
 }
---
+++ src/main/java/RegexThis.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexThis {
+    //3 to 5 lines of regex
+    private String orig;
+    public static String regexer(String raw){
+        
+        return raw;
+    }
+
+}
